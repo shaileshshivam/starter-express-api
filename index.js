5712716a44e6115c84f98d35be7df324101fdb65
Shailesh Shivam <shaileshshivam.dev@gmail.com>
2023-06-19 02:48:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,18 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.get("/api/trustloop/consent/:companyId", (req, res) => {
+  const companyId = req.params.companyId
+  console.log(req.query)
+
+  console.log({
+    status: req.query.status,
+    consentID: req.query.status,
+  })
+
+  res.json({}).end();
+})
+
+
 app.listen(process.env.PORT || 3000)
